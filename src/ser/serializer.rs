e2ab20bb4c67033916fe0b2e8273dc753f33814a
dignifiedquire <me@dignifiedquire.com>
2023-01-25 19:55:05 +0100

    cleanup

--- src/ser/serializer.rs
+++ src/ser/serializer.rs
@@ -547,18 +547,18 @@ where
     }
 }
 
-pub(crate) fn zig_zag_i16(n: i16) -> u16 {
+fn zig_zag_i16(n: i16) -> u16 {
     ((n << 1) ^ (n >> 15)) as u16
 }
 
-pub(crate) fn zig_zag_i32(n: i32) -> u32 {
+fn zig_zag_i32(n: i32) -> u32 {
     ((n << 1) ^ (n >> 31)) as u32
 }
 
-pub(crate) fn zig_zag_i64(n: i64) -> u64 {
+fn zig_zag_i64(n: i64) -> u64 {
     ((n << 1) ^ (n >> 63)) as u64
 }
 
-pub(crate) fn zig_zag_i128(n: i128) -> u128 {
+fn zig_zag_i128(n: i128) -> u128 {
     ((n << 1) ^ (n >> 127)) as u128
 }
