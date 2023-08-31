b58972cda1bdf2fc7c529e606a817745d7e8df62
Caleb Jamison <caleb@cbjamo.com>
2023-08-30 20:07:52 -0400

    Add specific error for Crc errors. (#112)

--- tests/crc.rs
+++ tests/crc.rs
@@ -37,3 +37,24 @@ fn test_crc_8() {
     let remaining_bytes = [];
     assert_eq!(res, (expected_bytes, remaining_bytes.as_slice()));
 }
+
+#[test]
+#[cfg(feature = "use-crc")]
+fn test_crc_error() {
+    use crc::{Crc, CRC_32_ISCSI};
+
+    let data: &[u8] = &[0x01, 0x00, 0x20, 0x30];
+    let buffer = &mut [0u8; 32];
+    let crc = Crc::<u32>::new(&CRC_32_ISCSI);
+    let digest = crc.digest();
+    let res = postcard::to_slice_crc32(data, buffer, digest).unwrap();
+
+    // intentionally corrupt the crc
+    let last = res.len() - 1;
+    res[last] = 0;
+
+    let digest = crc.digest();
+    let res = postcard::take_from_bytes_crc32::<[u8; 5]>(&res, digest);
+
+    assert_eq!(res, Err(postcard::Error::DeserializeBadCrc));
+}
