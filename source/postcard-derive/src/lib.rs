c0c1ded58d0351dcd380c2c8f8f6b5cdab28f4ac
Julien Cretin <cretin@google.com>
2024-05-14 15:56:28 +0200

    Add #[postcard(bound = "...")] attribute for derive(Schema)

--- source/postcard-derive/src/lib.rs
+++ source/postcard-derive/src/lib.rs
@@ -1,3 +1,5 @@
+use syn::{parse_macro_input, DeriveInput};
+
 mod max_size;
 mod schema;
 
@@ -8,7 +10,10 @@ pub fn derive_max_size(item: proc_macro::TokenStream) -> proc_macro::TokenStream
 }
 
 /// Derive the `postcard::Schema` trait for a struct or enum.
-#[proc_macro_derive(Schema)]
+#[proc_macro_derive(Schema, attributes(postcard))]
 pub fn derive_schema(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    schema::do_derive_schema(item)
+    let input = parse_macro_input!(item as DeriveInput);
+    schema::do_derive_schema(input)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
 }
