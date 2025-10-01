d56f5ad7182a2d0f55256a5efc94ecda66f7579f
Ross Lannen <ross.lannen@gmail.com>
2025-10-01 10:01:09 -0600

    Implement schema for nalgebra v0.34.

--- source/postcard-schema/src/impls/mod.rs
+++ source/postcard-schema/src/impls/mod.rs
@@ -41,6 +41,10 @@ pub mod heapless_v0_9;
 #[cfg_attr(docsrs, doc(cfg(feature = "nalgebra-v0_33")))]
 pub mod nalgebra_v0_33;
 
+#[cfg(feature = "nalgebra-v0_34")]
+#[cfg_attr(docsrs, doc(cfg(feature = "nalgebra-v0_34")))]
+pub mod nalgebra_v0_34;
+
 #[cfg(feature = "serde-big-array-v0_5")]
 #[cfg_attr(docsrs, doc(cfg(feature = "serde-big-array-v0_5")))]
 pub mod serde_big_array_v0_5;
