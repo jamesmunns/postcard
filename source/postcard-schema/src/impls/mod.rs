9c0dad9b66eab4849bffa704c44eb2e7d4a9d030
William Smith <calsignlabs@gmail.com>
2025-10-31 14:37:33 -0700

    Implement postcard_schema::Schema for bytes::Bytes

--- source/postcard-schema/src/impls/mod.rs
+++ source/postcard-schema/src/impls/mod.rs
@@ -53,6 +53,10 @@ pub mod serde_big_array_v0_5;
 #[cfg_attr(docsrs, doc(cfg(feature = "uuid-v1_0")))]
 pub mod uuid_v1_0;
 
+#[cfg(feature = "bytes-v1_0")]
+#[cfg_attr(docsrs, doc(cfg(feature = "bytes-v1_0")))]
+pub mod bytes_v1_0;
+
 impl Schema for NamedType {
     const SCHEMA: &'static NamedType = &NamedType {
         name: "NamedType",
