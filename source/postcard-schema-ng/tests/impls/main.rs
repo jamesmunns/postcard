bee18a4adfc2f45d338477dc14dbeb4a3fa9446d
William Smith <calsignlabs@gmail.com>
2025-10-31 14:36:54 -0700

    Allow round-trip integration tests for schemas

---
+++ source/postcard-schema-ng/tests/impls/main.rs
@@ -0,0 +1,16 @@
+use std::cmp::PartialEq;
+use std::fmt::Debug;
+
+use postcard_schema_ng::{schema::owned::OwnedDataModelType, Schema};
+use serde::{de::DeserializeOwned, Serialize};
+
+fn round_trip_test<T>(val: T)
+where
+    T: Serialize + DeserializeOwned + Schema + PartialEq + Debug,
+{
+    let schema = OwnedDataModelType::from(T::SCHEMA);
+    let encoded = postcard::to_allocvec(&val).unwrap();
+    let value = postcard_dyn_ng::from_slice_dyn(&schema, &encoded).unwrap();
+    let decoded: T = serde_json::from_value(value).unwrap();
+    assert_eq!(val, decoded);
+}
