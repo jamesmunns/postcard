074f228f6570f04e4ce6d9feaa071ec9b189190e
Luna Borowska <luna@borowska.pw>
2026-07-12 00:42:23 +0200

    Implement Schema for enum_map::EnumMap

--- source/postcard-schema-ng/src/impls/mod.rs
+++ source/postcard-schema-ng/src/impls/mod.rs
@@ -18,6 +18,10 @@ pub mod builtins_std;
 #[cfg_attr(docsrs, doc(cfg(feature = "chrono-v0_4")))]
 pub mod chrono_v0_4;
 
+#[cfg(feature = "enum-map-v3_0")]
+#[cfg_attr(docsrs, doc(cfg(feature = "enum-map-v3_0")))]
+pub mod enum_map_v3_0;
+
 #[cfg(feature = "fixed-v1_0")]
 #[cfg_attr(docsrs, doc(cfg(feature = "fixed-v1_0")))]
 pub mod fixed_v1_0;
